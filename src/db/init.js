8f55ad0c0d4c7e357d4627fcdf8058aa961d639b
Robin Rudén <robinruden@gmail.com>
2025-08-02 12:15:54 +0200

    inserted a console log in init.js

--- src/db/init.js
+++ src/db/init.js
@@ -7,6 +7,7 @@ const userDataDir = app.getPath('userData');
 const dbFile = path.join(userDataDir, 'audio_analysis.db');
 
 const db = new Database(dbFile);
+console.log('🔍 Opening SQLite at', dbFile);
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS tracks (
